1ce6524cc7f6f0d97516fd58f793f189dfc29dbe
Slawomir Borowiec <przodownik@tlen.pl>
2015-04-14 23:45:03 +0200

    lombok test added

---
+++ src/main/java/pl/java/scalatech/domain/_new/PersonNew.java
@@ -0,0 +1,12 @@
+package pl.java.scalatech.domain._new;
+
+/**
+ * @author Sławomir Borowiec 
+ * Module name : university_training
+ * Creating time :  14 kwi 2015 23:41:55
+ 
+ */
+
+public class PersonNew {
+
+}
---
+++ src/main/java/pl/java/scalatech/domain/type/Sex.java
@@ -0,0 +1,11 @@
+package pl.java.scalatech.domain.type;
+
+/**
+ * @author Sławomir Borowiec 
+ * Module name : university_training
+ * Creating time :  14 kwi 2015 23:36:58
+ 
+ */
+public enum Sex {
+ MALE,FEMALE;
+}
