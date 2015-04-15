06ade4eee187adb112cf07f2e64ea5a3d741e200
Slawomir Borowiec <przodownik@tlen.pl>
2015-04-15 19:45:36 +0200

    NonNull @Setter & constructor & field test

--- src/main/java/pl/java/scalatech/domain/_new/PersonNew.java
+++ src/main/java/pl/java/scalatech/domain/_new/PersonNew.java
@@ -3,6 +3,7 @@ package pl.java.scalatech.domain._new;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
 
 /**
  * @author Sławomir Borowiec
@@ -13,12 +14,23 @@ import lombok.NonNull;
 public class PersonNew {
     @Getter
     private String login;
-
+    
+    @NonNull @Getter @Setter
     private String name;
 
-    public PersonNew(@NonNull String login) {
+    
+    //public PersonNew(@NonNull String login) {
+      //  this.login = login;
+    //}
 
+    public PersonNew(@NonNull String login, String name) {
         this.login = login;
+        this.name = name;
     }
 
+
+    String login
+
+   
+    
 }
--- src/test/java/pl/java/scalatech/test/domain/lombok/PersonLombokTest.java
+++ src/test/java/pl/java/scalatech/test/domain/lombok/PersonLombokTest.java
@@ -5,11 +5,26 @@ import org.junit.Test;
 import pl.java.scalatech.domain._new.PersonNew;
 
 public class PersonLombokTest {
-    @Test(expected=NullPointerException.class)
+    @Test(expected = NullPointerException.class)
     public void shouldNotNullWork() {
         String name = null;
-        PersonNew p = new PersonNew(name);
-        
+        PersonNew p = new PersonNew(name, "test");
 
+    }
+
+    @Test
+    public void shouldNotNullFieldWork() {
+        String name = "slawek";
+        PersonNew p = new PersonNew(name, "test");
+        p.getName();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldNotNullSetter() {
+        String login = "slawek";
+        String name = null;
+        PersonNew p = new PersonNew(login, "something");
+        p.setName(name);
+        // p.getName();
     }
 }
