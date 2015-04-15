9907981932218c6802d4440a421e07ed12648570
Slawomir Borowiec <przodownik@tlen.pl>
2015-04-15 21:45:13 +0200

    renamed package

---
+++ src/main/java/pl/java/scalatech/domain/new_one/PersonNew.java
@@ -0,0 +1,12 @@
+package pl.java.scalatech.domain.new_one;
+
+/**
+ * @author Sławomir Borowiec 
+ * Module name : university_training
+ * Creating time :  14 kwi 2015 23:41:55
+ 
+ */
+
+public class PersonNew {
+
+}
