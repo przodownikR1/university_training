b7549df4cf4f1bde3f0325da5772654f8fb4f807
Slawomir Borowiec <przodownik@tlen.pl>
2015-06-18 13:25:36 +0200

    range test + optional /null ->String test added

--- src/test/java/pl/java/scalatech/test/OptionalTest.java
+++ src/test/java/pl/java/scalatech/test/OptionalTest.java
@@ -1,28 +1,34 @@
 package pl.java.scalatech.test;
 
+import java.util.List;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.fest.assertions.Assertions;
 import org.junit.Test;
 
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Optional;
+import com.google.common.base.Strings;
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 
+@Slf4j
 public class OptionalTest {
     private Multimap<String, String> map = ArrayListMultimap.create();
+
     // map null
     public OptionalTest() {
         map.put("yamaha", "R1");
         map.put("suzuki", "gsxr");
     }
-       
+
     public void oldWay() {
         String moto = map.get("yamaha").toString().toUpperCase();
         System.out.println(moto);
@@ -54,7 +60,22 @@ public class OptionalTest {
         Item i = null;
         Optional<Item> opI = Optional.fromNullable(i);
         Assertions.assertThat(opI.isPresent()).isFalse();
-        
+
+    }
+
+    @Test
+    public void test2() {
+        Optional<Integer> optional = Optional.absent();
+        Integer or = optional.or(2);
+        Assertions.assertThat(optional.isPresent()).isFalse();
+        Assertions.assertThat(or).isEqualTo(2);
+    }
+
+    @Test
+    public void testNull() {
+        List<String> persons = Lists.newArrayList("A", null, "C", "przodownik", null);
+        persons.stream().forEach(p -> log.info("{} , {} ", p, Strings.nullToEmpty(p)));
+
     }
 
     private UserP person = null;
@@ -64,10 +85,12 @@ public class OptionalTest {
         Optional<UserP> p = Optional.fromNullable(person);
         Assertions.assertThat(MoreObjects.firstNonNull(person, "")).isEqualTo("");
     }
+
     @Data
     class UserP {
         private String name;
-     }
+    }
+
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
